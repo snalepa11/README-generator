6e68e2390346a1d444de71e3530b602758b35dd1
Snalepa <snalepa1998@gmail.com>
2024-02-17 15:45:31 -0500

    Started on genertateMardown file

---
+++ assets/generateMarkdown.js
@@ -0,0 +1,30 @@
+
+/*
+{
+    1: hello bob
+    2. name of author 
+}
+*/
+
+function generateMarkdown(data){
+    console.log(data)
+    `#Title 
+    ##Table of Contents
+
+    ##Description 
+
+    ##Installation Instructions
+
+    ##Usage
+
+    ##Contributions
+
+    ##Test Instructions
+
+    ##License 
+
+    ##Questions
+    - GitHub Name: 
+    - GitHub Link: 
+    - Email:`
+}
---
+++ assets/index.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+let generateMarkdown = require('./assets/generateMarkdown');
+
+
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "Enter the title of your project"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please enter a description of your project"
+    },
+    {
+        type: "input",
+        name: "Installastion Instructions",
+        message:"Enter installation instructions if applicable."
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please enter usage information"
+    },
+    {
+        type: "input",
+        name: "Contributions",
+        message: "Please enter any contributers"
+    },
+    {
+        type: "input",
+        name: "Test Instructions",
+        message: "Please enter test instructions."
+    },
+    {
+        type: "choice",
+        name: "License",
+    },
+    {
+        type: "input",
+        name: "Questions",
+        message: "Please enter your GitHub username, GitHub profile link and email address."
+    },
+    {
+        type: "input",
+        name: "Questions",
+        message: "Enter the link to your GitHub."
+    },
+    {
+        type: "input",
+        name: "Questions",
+        message: "Enter your email address."
+    }
+]
+
+function wrtieToFile(fileName, data){
+    return fs
+}
+
+var inquirer = require('inquirer');
+ inquirer.prompt(questions).then((answers) => {
+      console.log('Creating README.md File...'); 
+      wrtieToFile("./README.md", generateMarkdown({...answers}))
+   })
+   .catch((error) => {
+      if (error.isTtyError) {
+     console.log('Prompt could not be rendered in the current environment') 
+     } else {
+       
+     }
+   });
